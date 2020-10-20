3fbc61605298f24e1cb9ef6456839ac4c5db7031
cyrus <rui.xi@huilianyi.com>
2020-10-20 14:25:55 +0800

    first commit

---
+++ src/test/basicTypes/Main.java
@@ -0,0 +1,16 @@
+package test.basicTypes;
+
+import java.util.ArrayList;
+
+/**
+ * @author: Cyrus
+ * @date: 2020/10/20 11:30 上午
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+    }
+}
